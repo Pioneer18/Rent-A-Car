feea0ba32bf87ebb0cd1e55136df9949e9ee1b4e
Jonathan Sells <sellsj14@gmail.com>
2020-02-15 11:53:31 -0500

    applying tslint corrections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './service/app.service';
 import { VehicleModule } from '../vehicle/vehicle.module';
 import { UserModule } from '../user/user.module';
 import { MongooseModule } from '@nestjs/mongoose'
+import { RouterModule } from 'nest-router';
+import { routes} from '../common/routes';
 
 @Module({
   imports: [AppModule, VehicleModule, UserModule],
