4722d454e2a567ca764ac0c30f29348c4d7c1bba
Jonathan Sells <sellsj14@gmail.com>
2020-02-15 01:33:03 -0500

    created a app dir to organize the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppModule } from './app/app.module';
 
 @Module({
-  imports: [],
+  imports: [AppModule],
   controllers: [AppController],
   providers: [AppService],
 })
