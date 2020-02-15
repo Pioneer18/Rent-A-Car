0bbaa6c77215ced16b5ec87ebba822559e77b917
Jonathan Sells <sellsj14@gmail.com>
2020-02-15 14:03:51 -0500

    templating the vehicle controller

--- src/vehicle/controller/vehicle.controller.ts
+++ src/vehicle/controller/vehicle.controller.ts
@@ -1,4 +1,31 @@
 import { Controller } from '@nestjs/common';
+import { VehicleService } from '../service/vehicle.service';
 
 @Controller('vehicle')
-export class VehicleController {}
+export class VehicleController {
+    constructor(private readonly vehicleService: VehicleService) {}
+}
+/**
+ * Create Rental:
+ * create a new vehcile rental listing
+ */
+
+ /**
+  * Search Rental:
+  * find rentals available near a specified location (user's location)
+  */
+
+/**
+ * Edit Pricing:
+ * edit the rental price
+ */
+
+/**
+ * Edit Vehicle Details:
+ * edit the details of the vehicle (# of seats, color, etc.)
+ */
+
+/**
+ * Schedule Unavailability
+ * set a period of unavailability for the rental (e.g. mon - wed  )
+ */
