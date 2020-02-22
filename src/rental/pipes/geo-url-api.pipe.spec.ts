eeac560ecd87c094a86c6b5d7f796452ec22c48d
Jonathan Sells <sellsj14@gmail.com>
2020-02-22 15:30:00 -0500

    updated the geo-url-api.pipe.ts file and it's test file

---
+++ src/rental/pipes/geo-url-api.pipe.spec.ts
@@ -0,0 +1,94 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { GeoUrlApiPipe } from './geo-url-api.pipe';
+import { GeoUrlApiUtil } from '../utils/geo-url-api.util';
+import { CreateRentalDto } from '../dto/create-rental-dto';
+/**
+ * What does this pipe do?
+ * Summary:
+ * Creates a single address string from the incoming CreateRentalDto
+ * Cleans up the string and makes an API request for the corresponding coordinates
+ * Returns the Coordinates, Address string, and the Original CreateRentalDto
+ * Mocks:
+ * mock the API return (which should be a provider( YUP))
+ */
+describe('GeoUrlApiPipe Unit Test', () => {
+
+    let util: GeoUrlApiUtil;
+    let pipe: GeoUrlApiPipe;
+    beforeAll(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [GeoUrlApiPipe, GeoUrlApiUtil],
+        }).compile();
+
+        util = module.get<GeoUrlApiUtil>(GeoUrlApiUtil);
+        pipe = module.get<GeoUrlApiPipe>(GeoUrlApiPipe);
+    });
+
+    describe('Check Definition', () => {
+        it('should have all necessary dependencies defined', () => {
+            expect(pipe).toBeDefined();
+            expect(util).toBeDefined();
+        });
+    });
+
+    describe('Test the Functionality of the Pipe', () => {
+        it('should return the original value, an address, and coordinates', async () => {
+            // mock coordinates
+            const mockedApiReturn: [number, number] = [37.786152, -79.443008];
+            // spy on the GeoUrlApiPipe
+            jest.spyOn(pipe, 'getCoordinates').mockImplementation(async () => mockedApiReturn);
+            // raw value
+            const createRentalDto: CreateRentalDto = {
+                specs: {
+                    odometer: 230000,
+                    transmission: 'Automatic',
+                    cityMpg: 28,
+                    hwyMpg: 33,
+                    fuel: 'gas',
+                    gasGrade: 'regular',
+                    description: 'Tokyo grocery getter',
+                    model: 'Honda Fit',
+                    style: 'sport',
+                    color: 'white',
+                    numOfSeats: 5,
+                    numDoors: 4,
+                    driveAssist: false,
+                    rearviewCam: false,
+                    bluetooth: false,
+                    sunRoof: false,
+                  },
+                  registration: {
+                    vin: 'E5597C6347',
+                    licensePlate: 'J51Y45',
+                    state: 'FL',
+                  },
+                  features: ['A/C', 'AUX'],
+                  scheduling: {
+                    requiredNotice: 4,
+                    rentMinDuration: 4,
+                    rentMaxDuration: 1,
+                  },
+                  pricing: {
+                    price: 28,
+                    discounts: {
+                      weekly: 0,
+                      monthly: 0,
+                    },
+                  },
+                  location: {
+                    street: '204 W Washington St',
+                    city: 'Lexington',
+                    zip: 24450,
+                  },
+                  photos: [],
+                  listed: true,
+            };
+            const address: string = `${createRentalDto.location.street} ${createRentalDto.location.city} ${createRentalDto.location.zip}`;
+            const test = await pipe.transform(createRentalDto);
+            expect(test.value).toEqual(createRentalDto);
+            expect(test.address).toEqual(address);
+            expect(test.coords).toEqual(mockedApiReturn);
+        });
+    });
+
+});
--- src/rental/pipes/geo-url-api.pipe.ts
+++ src/rental/pipes/geo-url-api.pipe.ts
@@ -12,18 +12,30 @@ export class GeoUrlApiPipe {
   private readonly appId: string;
   private readonly appCode: string;
   private readonly geoUrl: string;
+
   constructor(private readonly geoUrlApiUtil: GeoUrlApiUtil) {
     this.appId = process.env.GEO_ID;
     this.appCode = process.env.GEO_CODE;
     this.geoUrl = process.env.GEO_URL;
   }
 
-  async transform(value: CreateRentalDto, metadata: ArgumentMetadata) {
+  async createAddress(value) {
+    // create address string from incoming vehicle.address document
+    const address: string = `${value.location.street} ${value.location.city} ${value.location.zip}`;
+    return address;
+  }
+
+  async getCoordinates(address, appCode, appId, geoUrl) {
+    const coords = await this.geoUrlApiUtil.getCoordinates(address, appCode, appId, geoUrl);
+    return coords;
+  }
+
+  async transform(value: CreateRentalDto) {
     try {
-      // create address string from incoming vehicle.address document
-      const address: string = `${value.location.street} ${value.location.city} ${value.location.zip}`;
-      // request the coordinates
-      const coords = await this.geoUrlApiUtil.getCoordinates(address, this.appCode, this.appId, this.geoUrl);
+      // create the address
+      const address = await this.createAddress(value);
+      // request the coordinates from the API
+      const coords = await this.getCoordinates(address, this.appCode, this.appId, this.geoUrl);
       return { value, coords, address };
     } catch (err) {
       throw new Error(err);
