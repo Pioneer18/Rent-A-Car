1c2c11285f175effad04e8df1a087a749c7b4fb6
Jonathan Sells <sellsj14@gmail.com>
2020-02-22 17:21:50 -0500

    update the joi-validation.pipe.ts file and created its test file

--- src/common/pipes/joi-validation.pipe.ts
+++ src/common/pipes/joi-validation.pipe.ts
@@ -1,15 +1,15 @@
-import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException, Logger } from '@nestjs/common';
+import { PipeTransform, Injectable, BadRequestException, Logger } from '@nestjs/common';
 import { Schema } from '@hapi/joi';
 
 /**
  * This class accepts a joi validationSchema when instantiated
- * It will validate an incoming value aginst the provided validation schema
+ * It will validate an incoming value against the provided validation schema
  */
 @Injectable()
 export class JoiValidationPipe implements PipeTransform {
   constructor(private readonly schema: Schema) {}
 
-  transform(value: any, metadata: ArgumentMetadata) {
+  transform(value: any) {
     const { error } = this.schema.validate(value);
     if (error) {
       Logger.error(error);
---
+++ src/rental/schema/validation/create-rental-validation.spec.ts
@@ -0,0 +1,56 @@
+import { MappedRentalInterface } from '../../../rental/interface/mapped-rental.interface';
+import { JoiValidationPipe } from '../../../common/pipes/joi-validation.pipe';
+import { CreateRentalValidation } from '../validation/create-rental-validation.schema';
+
+describe('Check Validation is correct', () => {
+  it('should only accept objects that fit the MapRentalInterface', () => {
+    const rental: MappedRentalInterface = {
+      rentalDescription: 'Tokyo grocery getter',
+      address: '204 W Washington St, Lexington, VA 24450',
+      specs: {
+        odometer: 230000,
+        transmission: 'Automatic',
+        cityMpg: 28,
+        hwyMpg: 33,
+        fuel: 'gas',
+        gasGrade: 'regular',
+        description: 'This is a tokyo grocery getter',
+        model: 'Honda Fit',
+        style: 'sport',
+        color: 'yellow',
+        numOfSeats: 5,
+        numDoors: 4,
+        driveAssist: false,
+        rearviewCam: false,
+        bluetooth: false,
+        sunRoof: false,
+      },
+      registration: {
+        vin: '511892000',
+        licensePlate: 'J51Y45',
+        state: 'FL',
+      },
+      features: ['A/C', 'AUX'],
+      scheduling: {
+        requiredNotice: 4,
+        rentMinDuration: 4,
+        rentMaxDuration: 1,
+      },
+      pricing: {
+        price: 28,
+        discounts: {
+          weekly: 0,
+          monthly: 0,
+        },
+      },
+      photos: [],
+      loc: {
+        type: 'Point',
+        coordinates: [37.786152, -79.443008],
+      },
+      listed: true,
+    };
+    const check = new JoiValidationPipe(CreateRentalValidation);
+    expect(check.transform(rental)).toEqual(rental);
+  });
+});
