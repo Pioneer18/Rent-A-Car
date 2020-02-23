9967aeac8964367377f09693b308028fa9c197d7
Jonathan Sells <sellsj14@gmail.com>
2020-02-23 00:42:26 -0500

    added createRentalQuery method to RentalService

--- src/rental/service/rental.service.ts
+++ src/rental/service/rental.service.ts
@@ -3,12 +3,42 @@ import { MappedRentalInterface } from '../interface/mapped-rental.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { RentalInterface } from '../interface/rental.interface';
+import { SearchRentalDto } from '../dto/search-rental.dto';
 
 @Injectable()
 export class RentalService {
   constructor(
     @InjectModel('Rental') private readonly rentalModel: Model<RentalInterface>,
   ) {}
+
+  private async createRentalQuery(rental) {
+    const query: any = {
+      'scheduling.rentMinDuration': { $lte: rental.rentalDuration },
+      'scheduling.rentMaxDuration': { $gte: rental.rentalDuration },
+      'scheduling.requiredNotice': { $lte: rental.givenNotice },
+      'loc': {
+          $near: {
+              $maxDistance: 12875, // 8 miles
+              $geometry: {
+                  type: rental.loc.type,
+                  coordinates: [
+                      rental.loc.coordinates[0], // latitude
+                      rental.loc.coordinates[1], // longitude
+                  ],
+              },
+          },
+      },
+  };
+    rental.price
+      ? query.pricing = {
+          price: rental.price, // add price as optional search parameter
+      }
+      : (rental.priceRange = null);
+    rental.features
+      ? (query.features = { $in: rental.features })
+      : (rental.features = null);
+    return query;
+  }
 
   /**
    * Create Rental:
@@ -28,8 +58,18 @@ export class RentalService {
    * Search Rental:
    * find rentals available near a specified locations (user's location)
    */
-  async searchRental(rental: any /*SearchRentalDto*/) {
-    //
+  async searchRental(rental: SearchRentalDto) {
+    try {
+      const query = await this.createRentalQuery(rental);
+      const rentals = await this.rentalModel.find(query);
+      if (rentals.length > 0) {
+        return rentals;
+      } else {
+        throw new Error('No rentals were found');
+      }
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 
   /**
