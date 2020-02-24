016edc9c8bad2bfa5fd85ab573875cfc301a16df
Jonathan Sells <sellsj14@gmail.com>
2020-02-23 21:05:54 -0500

    updating rental-duration.pipe.ts file

--- src/rental/pipes/rental-duration.pipe.spec.ts
+++ src/rental/pipes/rental-duration.pipe.spec.ts
@@ -1,3 +1,11 @@
+import { TestingModule, Test } from '@nestjs/testing';
+import { RentalDurationPipe } from './rental-duration.pipe';
+import { RentalDurationDto } from '../dto/rental-duration.dto';
+import { PostGivenNoticeDto } from '../dto/post-given-notice.dto';
+import { DateTime } from 'luxon';
+import { GenerateRentalDurationEnumUtil } from '../utils/generate-rental-duration-enum.util';
+import { Logger } from '@nestjs/common';
+
 /**
  * Summary:
  * accepts a PostGivenNoticeDto and returns a RentalDurationDto
@@ -9,7 +17,47 @@
  * expect correct rentalDuration
  */
 describe('RentalDurationPipe Unit Test', () => {
-    it('should..', () => {
-        // do stuffs
+
+    let app: TestingModule;
+    let pipe: RentalDurationPipe;
+
+    beforeAll(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [RentalDurationPipe, GenerateRentalDurationEnumUtil],
+        }).compile();
+        app = module;
+        pipe = module.get<RentalDurationPipe>(RentalDurationPipe);
+    });
+
+    describe('transform method unit test', () => {
+        it('should return a RentalDuration', async () => {
+            const mockPostGivenNoticeDto: PostGivenNoticeDto = {
+                address: '204 W Washington St Lexington 24450',
+                rentalStartTime: DateTime.fromISO(new Date('February 25, 2020 10:46:00').toISOString()),
+                rentalEndTime: DateTime.fromISO(new Date('February 28 , 2020 20:24:00').toISOString()),
+                price: null,
+                features: null,
+                givenNotice: 2,
+            }
+            const mockRentalDurationDto: RentalDurationDto = {
+                address: '204 W Washington St Lexington 24450',
+                rentalDuration: 3,
+                price: null,
+                features: null,
+                givenNotice: 2,
+            };
+            const test = await pipe.transform(mockPostGivenNoticeDto);
+            Logger.log('the test results below');
+            Logger.log(test);
+            expect(test.address).toBe(mockRentalDurationDto.address);
+            expect(test.features).toBe(mockRentalDurationDto.features);
+            expect(test.givenNotice).toBe(mockRentalDurationDto.givenNotice);
+            expect(test.price).toBe(mockRentalDurationDto.price);
+            expect(test.rentalDuration).toBe(mockRentalDurationDto.rentalDuration);
+        });
+    });
+
+    afterAll(async () => {
+        app.close();
     });
 });
