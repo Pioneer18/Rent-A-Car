ebac7fb79a163e90237d5e2c535c8b24f94f4e64
Jonathan Sells <sellsj14@gmail.com>
2020-03-08 18:21:20 -0400

    building out the images.controller

--- src/images/controller/images.controller.ts
+++ src/images/controller/images.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, Res } from '@nestjs/common';
+import { ImagesService } from '../service/images.service';
 
 @Controller('images')
-export class ImagesController {}
+export class ImagesController {
+    constructor(private readonly imagesService: ImagesService) {}
+    @Post('rentals')
+    async uploadRentalImages(@Req() req, @Res() res) {
+        return 'tee-hee';
+    }
+}
