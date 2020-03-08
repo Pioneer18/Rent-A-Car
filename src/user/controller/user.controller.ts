02b26a698e8c1ffd9dc48cf6b27352473a2d3e6c
Jonathan Sells <sellsj14@gmail.com>
2020-03-08 17:29:43 -0400

    templated out the start of the user controller

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor() {
+        // initialize stuffs
+    }
+    /**
+     * create profile
+     * update profile
+     * upload profile image
+     * login
+     * logout
+     * delete profile
+     */
+    @Post('create-profile')
+    async createProfile(@Body() profile) {
+        return 'tee-hee';
+    }
+
+    @Post('update-profile')
+    async updateProfile(@Body() update) {
+        return 'tee-hee';
+    }
+
+}
