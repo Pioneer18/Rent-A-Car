aa2237cab9b64a40e13b36c24ddfc0527c9d0f9f
Jonathan Sells <sellsj14@gmail.com>
2020-03-15 10:25:11 -0400

    updating the main.ts file

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
