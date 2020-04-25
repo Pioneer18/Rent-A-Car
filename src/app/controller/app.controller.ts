d4a9e39f3543a7b7894fbe318b78a3b009a6c002
Jonathan Sells <sellsj14@gmail.com>
2020-04-25 18:09:24 -0400

    added a LocalAuthGuard class and applied it to the 'auth/login' endpoint on the app.controller

--- src/app/controller/app.controller.ts
+++ src/app/controller/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Post, Request } from '@nestjs/common';
 import { AppService } from '../service/app.service';
+import { LocalAuthGuard } from '../../auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // bare-bones login route
+  // Passport automatically creates a user object, based on the value we return
+  // from the validate() method, and assigns it to the Request object as req.user.
+  // will replace this later to return a JWT instead
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
