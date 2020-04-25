208cdcc9fa1a97c4f8339b439f20eff0e29f1521
Jonathan Sells <sellsj14@gmail.com>
2020-04-25 16:26:51 -0400

    added the validateUser method to the auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import {UserService} from '../user/service/user.service';
 
+/**
+ * Passport Local
+ * Retrieve a user and verify their password with ValidateUser()
+ */
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userService: UserService) {}
+
+    async validateUser(username: string, pass: string): Promise<any> {
+        const user = await this.userService.findUser('tee-hee');
+        if (user && user.password === pass) {
+            const {password, ...result } = user;
+            // return everything but the password
+            return result;
+        }
+        return null;
+    }
+}
