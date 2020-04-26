f986e6252b2184ca7998ba1ebb061ec1affb844a
Jonathan Sells <sellsj14@gmail.com>
2020-04-26 15:26:42 -0400

    moved the app directory's contents into the src directory and deleted the old app directory

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, UseGuards, Post, Request } from '@nestjs/common';
-import { AppService } from '../service/app.service';
-import { LocalAuthGuard } from '../../auth/local-auth.guard';
+import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -18,6 +22,6 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
