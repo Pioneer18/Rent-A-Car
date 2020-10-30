3a28604d819f00147dc108af39a312befcc0d63a
User <user@users-imac.home>
2020-10-30 17:20:10 -0400

    updated the app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,10 @@
 import { Controller, Get, UseGuards, Post, Request } from '@nestjs/common';
-import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly authService: AuthService,
-  ) {}
-
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
+  constructor(private readonly authService: AuthService) {}
 
   // bare-bones login route
   // Passport automatically creates a user object, based on the value we return
