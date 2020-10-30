4d5d5e7264481841066f1ea80f153a2cf14a0551
User <user@users-imac.home>
2020-10-30 17:25:08 -0400

    added a user schema

---
+++ src/user/schema/user.schema.ts
@@ -0,0 +1,11 @@
+import * as mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const ObjectID = mongoose.ObjectID;
+
+export const UserSchema = new Schema({
+    userId: ObjectID,
+    username: String,
+    email: String,
+    password: String,
+})
