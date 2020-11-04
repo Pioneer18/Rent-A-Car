7800250b57323f1241e1265f733f3ea63fb528e5
User <user@users-imac.home>
2020-11-04 16:57:23 -0500

    finished the saveVehicleImages endpoint; it will save a single or multiple images to the db - need to update the reference logged in user functionality of this method still

--- src/images/service/images.service.ts
+++ src/images/service/images.service.ts
@@ -2,25 +2,48 @@
  * Save Images
  */
 import { Injectable} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { ImageInterface } from '../interface/image.interface';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ImagesService {
+  constructor(
+    @InjectModel('Images') private readonly imagesModel: Model<ImageInterface>,
+  ) {}
 
   // incoming files will always be in an array
   // map the files and upload them to the database
   async saveVehicleImages (files: [any]) {
-    console.log(files);
-    // map the files
-    files.map((item) => ({
-      /**
-       * for each item grab;
-       * - originalname
-       * - mimetype
-       * - buffer (main data)
-       * - size
-       * - userId (ref)
-       */
-    }))
-    return files;
+    try {
+      const packet: ImageInterface[] = []; 
+      // map the files and push to packet
+      // grab the logged in user's id from the jwt (must decode it)
+      files.map(item => {
+        /**
+         * for each item grab;
+         * - buffer (img data)
+         * - mimetype
+         * - originalname
+         * - encoding (e.g. 7bit)
+         * - size
+         * - userId (ref)
+         */
+        packet.push({
+          data: item.buffer,
+          mimeType: item.mimetype,
+          originalName: item.originalname,
+          encoding: item.encoding,
+          size: item.size,
+          user_id: 'fake_id_for_now23',
+        })
+      })
+      // use insertMany to save the packet to the db
+      const upload = await this.imagesModel.insertMany(packet);
+      console.log(`packet was inserted`);
+      return upload;
+    } catch(err) {
+      throw new Error(err);
+    }
   }
 }
