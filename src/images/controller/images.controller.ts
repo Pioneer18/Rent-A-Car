a51dc6e83edbd09b2ee773bb5af9edfae9dd51d7
User <user@users-imac.home>
2020-11-04 15:11:40 -0500

    commented out old code that used multer for AWS S3 Buckets, replacing with a UploadFiles endpoint, the files will be stored in MongoDB

--- src/images/controller/images.controller.ts
+++ src/images/controller/images.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Post, Req, Res, Logger } from '@nestjs/common';
+import { Controller, Post, Req, Res, Logger, UseInterceptors, UploadedFile, UploadedFiles } from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import { ImagesService } from '../service/images.service';
 
 @Controller('images')
 export class ImagesController {
     constructor(private readonly imagesService: ImagesService) {}
+    /*
     @Post('upload-rental-images')
     async uploadRentalImages(@Req() req, @Res() res) {
         const path = process.env.AWS_S3_BUCKET_RENTALS;
@@ -13,5 +15,24 @@ export class ImagesController {
     @Post('upload-profile-image')
     async uploadProfileImage(@Req() req, @Res() res) {
         const path = process.env.AWS_S3_BUCKET_PROFILE;
+    }
+    */
+
+    /**
+     * 1) upload vehicle photos to db with reference to the logged in user
+     * 2) upload profile photo to db with reference to the logged in user
+     * 3) remove specific photo from vehicle photo db
+     * 4) remove specific photo from profile photo db
+     */
+
+
+    /**
+     * upload a single or multiple photos to be saved
+     * @param files image files
+     */
+    @Post('upload-vehicle-images')
+    @UseInterceptors(FilesInterceptor('files'))
+    async uploadVehicleImages(@UploadedFiles() files: any) {
+        console.log(files);
     }
 }
