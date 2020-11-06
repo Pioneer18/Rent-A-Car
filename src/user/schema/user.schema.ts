be9874a8b07e870692bb3994680763c91efb5cdc
User <user@users-imac.home>
2020-11-06 09:48:36 -0500

    updated user.schema to require a unique email from each user

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -4,6 +4,9 @@ const Schema = mongoose.Schema;
 
 export const UserSchema = new Schema({
     username: String,
-    email: String,
+    email: { 
+        type: String, 
+        unique: true
+    },
     password: String,
 });
