d9446717012cdbb3916540d823bcb9364f2bdeaf
User <user@users-imac.home>
2020-11-06 12:53:53 -0500

    validateEmailMiddleware now queries Model

--- src/user/middleware/validate-email.middleware.ts
+++ src/user/middleware/validate-email.middleware.ts
@@ -1,16 +1,29 @@
 import { Inject, Injectable, NestMiddleware } from '@nestjs/common'
 import { Request, Response } from 'express';
+import { userModel } from '../../common/Const';
+import { UserInterface } from '../interface/user.interface';
+import { Model } from 'mongoose';
+import { CreateUserDto } from '../dto/create-user.dto';
 /**
  * Validate the requested email is unique to the database
  */
 @Injectable()
 export class ValidateEmailMiddleware implements NestMiddleware {
-    constructor() {}
+    constructor(
+        @Inject(userModel)
+        private readonly user: Model<UserInterface>,
+    ) {}
+
+    private async validateEmail(value: CreateUserDto){
+        const check = await this.user.find({email: 'nothing'});
+        console.log('The email check');
+        console.log(check);
+    }
 
     async use(req: Request, res: Response, next: Function) {
         // apply to create-user route
         if (req.originalUrl === 'v1/user/create-user') {
-            console.log('hello');
+            await this.validateEmail(req.body);
         }
         next();
     }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
-import { Module, NestModule } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from '../user/schema/user.schema'
 import { UserController } from './controller/user.controller';
 import { UserService } from './service/user.service';
 import { DatabaseModule } from '../database/database.module';
+import { ValidateEmailMiddleware } from './middleware/validate-email.middleware';
 
 
 @Module({
@@ -17,5 +18,10 @@ import { DatabaseModule } from '../database/database.module';
 })
 export class UserModule  {
   constructor() {
+  }
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ValidateEmailMiddleware)
+      .forRoutes('v1/user')
   }
 }
