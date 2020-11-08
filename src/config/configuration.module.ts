4e7edede88f239edb7eb7fda736941ab2007e930
User <user@users-imac.home>
2020-11-08 11:51:38 -0500

    udpated the configModule.forRoot{} validationSchema

--- src/config/configuration.module.ts
+++ src/config/configuration.module.ts
@@ -1,6 +1,7 @@
 import Joi = require("@hapi/joi");
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import { join } from "path";
 import configuration from "./configuration";
 import { AppConfigService } from "./configuration.service";
 /**
@@ -11,9 +12,9 @@ import { AppConfigService } from "./configuration.service";
         ConfigModule.forRoot({
             load: [configuration],
             validationSchema: Joi.object({
-                GEO_ID: Joi.string(),
-                GEO_CODE: Joi.string(),
-                GEO_URL: Joi.string(),
+                GEO_ID: Joi.string().required(),
+                GEO_CODE: Joi.string().required(),
+                GEO_URL: Joi.string().required(),
                 REMOTE_DB: Joi.string(),
                 LOCAL_DB: Joi.string(),
                 SECRET_KEY: Joi.string(),
@@ -21,6 +22,8 @@ import { AppConfigService } from "./configuration.service";
                   .valid('development', 'production', 'test')
                   .default('development'),
                 PORT: Joi.number().default(3000),
+                JWT_SECRET: Joi.string(), // required
+                JWT_EXPIRATION_TIME: Joi.string(),// required
             }),
             validationOptions: {
                 allowUnkown: false, // enforce validation, don't allow unknown keys in the env variables
