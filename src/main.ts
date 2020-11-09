136ddad3184f598dc51f1cac895b13e147d04648
User <user@users-imac.home>
2020-11-09 12:58:28 -0500

    setup the server as https

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,18 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { AppConfigService } from './config/configuration.service';
 import cookieParser = require('cookie-parser');
+import * as fs from 'fs';
 
 
 dotenv.config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('../https/private-key.pem'),
+    cert: fs.readFileSync('../https/public-certificate.pem')
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   const appConfig = await app.get(AppConfigService);
   app.use(cookieParser())
   app.enableCors();
