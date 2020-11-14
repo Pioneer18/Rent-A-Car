f0733ebdcb92597f8ec7c8a424c73a5fed46c08c
Pioneer18 <sellsj14@gmail.com>
2020-11-14 12:45:50 -0500

    updated the email.service

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -8,9 +8,10 @@ import * as smtpTransport from 'nodemailer-smtp-transport';
 export class EmailService {
     constructor() {}
 
+    // fallback mail options?
     mailOption = {
         from: 'rentacar.team2020@gmail.com',
-        to: '[user emai]',
+        to: 'rentacar.team2020@gmail.com',
         subject: 'Reset Forgotten Email Password for Rent-A-Car',
         text: 'Please select the reset email link to reset your password',
         html: '<h1>Rent-A-Car Password Reset</h1><br><p>please select the reset email link to reset your passowrd</p>'
@@ -28,4 +29,19 @@ export class EmailService {
             pass: 'Unathi2020!'
         }
     })
+
+    async createTransporter () {
+        // create a transporter from provided info
+    }
+
+    async sender(mailOption, sender): Promise<any> {
+        try{
+            const options = mailOption? mailOption : this.mailOption;
+            const mailer = sender? sender: this.transporter;
+
+            return await mailer.sendMail(options);
+        } catch(err) {
+            throw new Error(err);
+        }
+    }
 }
