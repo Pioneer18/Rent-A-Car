1fe4a3523225ab060cf7f2fe64981bc31ab8351c
Pioneer18 <sellsj14@gmail.com>
2020-11-14 13:54:57 -0500

    updated the email service

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 import * as smtpTransport from 'nodemailer-smtp-transport';
+import { MailOptionsInterface } from './interface/mailoptions.interface';
+import { TransporterInterface } from './interface/transporter.interface';
 /**
  * Email Service
  */
@@ -8,38 +10,40 @@ import * as smtpTransport from 'nodemailer-smtp-transport';
 export class EmailService {
     constructor() {}
 
-    // fallback mail options?
-    mailOption = {
-        from: 'rentacar.team2020@gmail.com',
-        to: 'rentacar.team2020@gmail.com',
-        subject: 'Reset Forgotten Email Password for Rent-A-Car',
-        text: 'Please select the reset email link to reset your password',
-        html: '<h1>Rent-A-Car Password Reset</h1><br><p>please select the reset email link to reset your passowrd</p>'
+
+    async createTransporter (): Promise<TransporterInterface> {
+        const transporter: TransporterInterface = nodemailer.createTransport({
+            host: 'smtp-mail.outlook.com',
+            secureConnection: true, // TLS requires secureConnection to be false
+            port: 587,
+            tls: {
+                ciphers: 'SSLv3'
+            },
+            auth: {
+                user: 'rentacar.team2020@gmail.com',
+                pass: 'Unathi2020!'
+            }
+        })
+        return transporter;
     }
-    
-    transporter = nodemailer.createTransport({
-        host: 'smtp-mail.outlook.com',
-        secureConnection: true, // TLS requires secureConnection to be false
-        port: 587,
-        tls: {
-            ciphers: 'SSLv3'
-        },
-        auth: {
-            user: 'rentacar.team2020@gmail.com',
-            pass: 'Unathi2020!'
+
+    /**
+     * @param email user's email
+     */
+    async createMailOptions (email: string): Promise<MailOptionsInterface> {
+        const mailOptions: MailOptionsInterface = {
+            from: 'rentacar.team2020@gmail.com',
+            to: email,
+            subject: 'Reset Forgotten Email Password for Rent-A-Car',
+            text: 'Please select the reset email link to reset your password',
+            html: '<h1>Rent-A-Car Password Reset</h1><br><p>please select the reset email link to reset your passowrd</p>'
         }
-    })
-
-    async createTransporter () {
-        // create a transporter from provided info
+        return mailOptions;
     }
 
-    async sender(mailOption, sender): Promise<any> {
+    async sendMail(mailOptions, sender): Promise<any> {
         try{
-            const options = mailOption? mailOption : this.mailOption;
-            const mailer = sender? sender: this.transporter;
-
-            return await mailer.sendMail(options);
+            sender.sendMail(mailOptions);
         } catch(err) {
             throw new Error(err);
         }
