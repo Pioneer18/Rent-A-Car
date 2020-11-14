3065c85b5bf6d178067ca6110652413fb8637377
Pioneer18 <sellsj14@gmail.com>
2020-11-14 12:37:57 -0500

    building out the email service

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import * as smtpTransport from 'nodemailer-smtp-transport';
 /**
  * Email Service
  */
@@ -6,5 +8,24 @@ import { Injectable } from "@nestjs/common";
 export class EmailService {
     constructor() {}
 
+    mailOption = {
+        from: 'rentacar.team2020@gmail.com',
+        to: '[user emai]',
+        subject: 'Reset Forgotten Email Password for Rent-A-Car',
+        text: 'Please select the reset email link to reset your password',
+        html: '<h1>Rent-A-Car Password Reset</h1><br><p>please select the reset email link to reset your passowrd</p>'
+    }
     
+    transporter = nodemailer.createTransport({
+        host: 'smtp-mail.outlook.com',
+        secureConnection: true, // TLS requires secureConnection to be false
+        port: 587,
+        tls: {
+            ciphers: 'SSLv3'
+        },
+        auth: {
+            user: 'rentacar.team2020@gmail.com',
+            pass: 'Unathi2020!'
+        }
+    })
 }
