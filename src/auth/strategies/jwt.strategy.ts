8f9883736f4a2e6eca9b727657198a0d3e3ec648
Pioneer18 <sellsj14@gmail.com>
2020-11-16 18:43:20 -0500

    udpated jwt-strategy to grab jwts from the cookie header

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,12 +3,16 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtConstants } from '../constant';
+import { Request } from 'express'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      // read JWT from the Cookie Header
+      jwtFromRequest: ExtractJwt.fromExtractors([(request: Request) => {
+        return request?.cookies?.Authentication;
+      }]),
       ignoreExpiration: false,
       secretOrKey: jwtConstants.secret,
     });
