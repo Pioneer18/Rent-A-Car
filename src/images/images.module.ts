e987e40d84ebb7a58141264f53b497857ff7c034
Pioneer18 <sellsj14@gmail.com>
2020-11-17 21:38:33 -0500

    added an aws s3 bucket uploader to the images service

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppConfigModule } from '../config/configuration.module';
+import { AppConfigService } from '../config/configuration.service';
 import { DatabaseModule } from '../database/database.module';
 import { ImagesController } from './controller/images.controller';
 import { ImageSchema } from './schema/images.schema';
@@ -8,10 +10,11 @@ import { ImagesService } from './service/images.service';
 @Module({
     imports: [
         DatabaseModule,
-        MongooseModule.forFeature([{ name: 'Images', schema: ImageSchema}])
+        MongooseModule.forFeature([{ name: 'Images', schema: ImageSchema}]),
+        AppConfigModule
     ],
     controllers: [ImagesController],
-    providers: [ImagesService],
+    providers: [ImagesService, AppConfigService],
     exports: [ImagesService]
 
 })
--- src/images/service/images.service.ts
+++ src/images/service/images.service.ts
@@ -10,18 +10,24 @@ import { JwtPayloadInterface } from '../../auth/interface/jwt-payload';
 import * as AWS from 'aws-sdk';
 import * as multer from 'multer';
 import* as multerS3 from 'multer-s3'
+import { AppConfigService } from '../../config/configuration.service';
 
-const s3 = new AWS.S3();
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
 
 @Injectable()
 export class ImagesService {
+
+  private s3
   constructor(
     @InjectModel('Images') private readonly imagesModel: Model<ImageInterface>,
-  ) {}
+    private readonly appConfig: AppConfigService,
+      ) {
+        this.s3 = new AWS.S3();
+        AWS.config.update({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    });
+
+      }
 
  /**
   * TODO: Save with specific [rental id] as well
@@ -109,7 +115,7 @@ export class ImagesService {
   private uploader = (req, res, path) => {
     const upload = multer({
       storage: multerS3({
-        s3,
+        s3: this.s3,
         bucket: 'rentals/', // process.env.AWS_S3_BUCKET_RENTALS
         acl: 'public-read',
         key: (request, file, cb) => {
