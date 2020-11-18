14661d61897e68fcab4bc16c75698772096139ec
Pioneer18 <sellsj14@gmail.com>
2020-11-17 21:04:53 -0500

    setting up the images service to be able to upload images to a public s3 bucket

--- src/images/service/images.service.ts
+++ src/images/service/images.service.ts
@@ -2,12 +2,20 @@
  * Save Images
  * Could upload images to AWS S3 bucket and save the links?
  */
-import { Injectable} from '@nestjs/common';
+import { Injectable, Logger, Req, Res} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { ImageInterface } from '../interface/image.interface';
 import { Model } from 'mongoose';
 import { JwtPayloadInterface } from '../../auth/interface/jwt-payload';
-import { UserPropertyInterface} from '../../auth/interface/user-property.interface';
+import * as AWS from 'aws-sdk';
+import * as multer from 'multer';
+import* as multerS3 from 'multer-s3'
+
+const s3 = new AWS.S3();
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 @Injectable()
 export class ImagesService {
@@ -91,6 +99,43 @@ export class ImagesService {
       return await this.imagesModel.deleteMany({ user_id: user.sub, category});
     } catch(err) {
       throw new Error(err);
+    }
+  }
+
+  /**
+   * build an upload document for the selected directory
+   * @param {string} directory the s3 bucket directory path
+   */
+  private uploader = (req, res, path) => {
+    const upload = multer({
+      storage: multerS3({
+        s3,
+        bucket: 'rentals/', // process.env.AWS_S3_BUCKET_RENTALS
+        acl: 'public-read',
+        key: (request, file, cb) => {
+          cb(null, `${Date.now().toString()} - ${file.originalname}`);
+        },
+      }),
+    }).array('upload', 1);
+
+    upload(req, res, error => {
+      if (error) {
+        return res.status(404).json(`Failed to upload image file: ${error}`);
+      }
+      return res.status(201).json(req.files[0].location);
+    });
+  }
+
+  async uploadImages(@Req() req, @Res() res, path) {
+    // upload to aws s3 bucket
+    try {
+      Logger.log('below is the req.body');
+      Logger.log(Object.keys(req));
+      Logger.log(req.headers);
+      // const path = process.env.AWS_S3_BUCKET_RENTALS;
+      this.uploader(req, res, path);
+    } catch (err) {
+      return res.status(500).json(`Failed to upload image file: ${err}`);
     }
   }
 
