427dc63080c45df3bc26e491ab62c5ce9682ae34
Pioneer18 <sellsj14@gmail.com>
2020-11-21 08:07:34 -0500

    updating the images.controller to work with multer and not the FilesInterceptor

--- src/images/controller/images.controller.ts
+++ src/images/controller/images.controller.ts
@@ -3,7 +3,7 @@ import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { AppConfigService } from '../../config/configuration.service';
 import { JwtAuthGuard } from '../../auth/gaurds/jwt-auth.guard';
 import { ImagesService } from '../service/images.service';
-import { Request } from 'express';
+import { Request, response } from 'express';
 
 @UseGuards(JwtAuthGuard)
 @Controller('images')
@@ -11,26 +11,24 @@ export class ImagesController {
     constructor(private readonly imagesService: ImagesService, private readonly appConfig: AppConfigService) { }
 
     /**
-     * 1) upload vehicle photos to db with reference to the logged in user
-     * 2) find vehicle photos in the db 
+     * 1) upload rental photos to db with reference to the logged in user
      * 3) upload profile photo to db with reference to the logged in user
+     * 2) find rental photos in the db 
      * 4) find profile photos in the db
-     * 5) remove specific photo(s) from vehicle photo db
-     * 6) remove specific photo(s) from profile photo db
+     * 5) remove specific rental photo(s) from db
+     * 6) remove specific profile photo(s) from db
      */
 
 
     /**
-     * Upload a single or multiple vehicle photos to be saved
+     * Upload a single or multiple rental photos to be saved
      * @param files fieldName
      * @param maxCount maxCount
      * @param options option
      */
-    @Post('upload-vehicle-images')
-    @UseInterceptors(FilesInterceptor('files'))
-    async uploadVehicleImages(@UploadedFiles() files: [any], @Req() req) {
-        // pass files to images service to be saved in the db
-        // return await this.imagesService.saveImages(files, req.user, 'Vehicle');
+    @Post('upload-rental-images')
+    async uploadRentalImages(@Req() req, @Res() res) {
+        // return await this.imagesService.multerUpload()
     }
 
     /**
@@ -38,9 +36,8 @@ export class ImagesController {
      * @param files fieldName
      */
     @Post('upload-profile-images')
-    @UseInterceptors(FilesInterceptor('file'))
-    async uploadProfileImage(@UploadedFiles() file, @Req() req) {
-        // return await this.imagesService.saveImages(file, req.user, 'Profile');
+    async uploadProfileImage(@Req() req, @Res() res) {
+        // return await this.imagesService.multerUpload()
     }
 
     /**
@@ -93,17 +90,35 @@ export class ImagesController {
     */
 
     /**
+     * DEPRECATED
      * Upload to AWS S3 Bucket
      * Bucket base: rent-a-car-photos/{email}/{vehicle or profile}
      * @param {string} file the file(s) being uploaded
      * @param {object} req.user the logged in user
      * @param {string} category rentals or profile
      * @param {string} rental_id id of the rental; if this is a rental image upload
-     */
     @Post('upload')
     @UseInterceptors(FilesInterceptor('files'))
     async upload(@UploadedFiles() files, @Req() req, @Body() rental_id) {
         return await this.imagesService.upload(files, req.user, 'test', rental_id);
+    }
+    */
+
+    /**
+     * Upload images to AWS S3 Bucket
+     * Bucket: rent-a-car-photos/{user_email}/{category}/{tag}/
+     * uses handler uses multer to extract file(s) from the Request
+     * TODO: This is 2 Routes - 1) Rentals and 2) Profile image(s) upload
+     */
+    @Post('multer-upload')
+    async multerUpload(@Req() req, @Res() res) {
+        try {
+            await this.imagesService.fileupload(req, res)
+        } catch(err) {
+            return response
+                .status(500)
+                .json(`Failed to upload image file: ${err.message}`)
+        }
     }
 
 }
