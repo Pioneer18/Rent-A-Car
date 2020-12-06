f9e19ce61addbb0f6f6718d35c2b7fa65ccb807f
Pioneer18 <sellsj14@gmail.com>
2020-12-05 22:38:49 -0500

    updating the images.service.spec.ts

--- src/images/service/images.service.spec.ts
+++ src/images/service/images.service.spec.ts
@@ -1,4 +1,14 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { AppConfigService } from '../../config/configuration.service';
+import { AppConfigModule } from '../../config/configuration.module';
+import { DatabaseModule } from '../../database/database.module';
+import { S3Provider } from '../providers/s3.provider';
+import { ImageSchema } from '../schema/images.schema';
+import { CreateMulterUploadUtil } from '../util/create-multer-upload.util';
+import { DeleteS3ImagesUtil } from '../util/delete-s3-images.util';
+import { MulterUploadUtil } from '../util/multer-upload.util';
+import { ProcessSaveDataUtil } from '../util/process-save-data.util';
 import { ImagesService } from './images.service';
 
 describe('ImagesService', () => {
@@ -6,7 +16,23 @@ describe('ImagesService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ImagesService],
+      imports: [
+        MongooseModule.forFeature([{ name: 'Images', schema: ImageSchema}]),
+        MongooseModule.forRoot('mongodb://localhost/rent-a-car-test', {
+          useNewUrlParser: true,
+        }),
+        AppConfigModule,
+        DatabaseModule
+    ],
+      providers: [
+        AppConfigService,
+        ImagesService,
+        ProcessSaveDataUtil,
+        CreateMulterUploadUtil,
+        MulterUploadUtil,
+        DeleteS3ImagesUtil,
+        S3Provider
+      ],
     }).compile();
 
     service = module.get<ImagesService>(ImagesService);
@@ -15,4 +41,5 @@ describe('ImagesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
 });
