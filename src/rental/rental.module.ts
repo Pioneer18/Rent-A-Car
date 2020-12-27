dce1004a78b4362ba8eec72ed3a57d56a54e44c1
Pioneer18 <sellsj14@gmail.com>
2020-12-27 10:54:00 -0500

    updated the rental.module by removing unavailability related code

--- src/rental/rental.module.ts
+++ src/rental/rental.module.ts
@@ -1,12 +1,10 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { RentalController } from './controller/rental.controller';
 import { RentalService } from './service/rental.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RentalSchema } from './schema/rental.schema';
 import { GeoUrlApiUtil } from './utils/geo-url-api.util';
 import { DatabaseModule } from '../database/database.module';
-// import { unavailabilityProvider } from '../database/providers/unavailability-model.provider';
-// import { ValidateUpdateUnavailabilityMiddleware } from './middleware/validate-update-unavailability.middleware';
 import { MapNewRentalPipe } from './pipes/map-new-rental.pipe';
 import { GeoUrlApiPipe } from './pipes/geo-url-api.pipe';
 import { RequestCoordinatesPipe } from './pipes/request-coordinates.pipe';
@@ -14,9 +12,6 @@ import { RentalDurationPipe } from './pipes/rental-duration.pipe';
 import { RentalSearchFilterPipe } from './pipes/rental-search-filter.pipe';
 import { PricingPipe } from './pipes/pricing.pipe';
 import { ValidateEditDetailsPipe } from './pipes/validate-edit-details.pipe';
-// import { ProcessUnavailabilityPipe } from './pipes/process-unavailability.pipe';
-// import { ValidateUnavailabilityPipe } from './pipes/validate-unavailability.pipe';
-// import { SortUnavailabilityPipe } from './pipes/sort-unavailability.pipe';
 import { CreateUpdaterDtoPipe } from './pipes/create-updater-dto.pipe';
 import { ValidateRemoveUnavailabilityPipe } from './pipes/validate-remove-unavailability.pipe';
 import { AppConfigService } from '../config/configuration.service';
@@ -29,7 +24,6 @@ import { RedisService } from '../redis/service/redis.service';
 import { RadiusToMeters } from './utils/radius-to-meters';
 /**
  * - **summary**: This module provides all of the functionality for working with Rentals
- * - **Middleware**: This module consumes the **ValidateUpdateUnavailability** middleware; for more details, in the documentation checkout the **Injectables** ValidateUpdateUnavailability tab
  * - **Database**: This module applies indexing to the **Unavailability** model of the database
  */
 @Module({
@@ -43,7 +37,6 @@ import { RadiusToMeters } from './utils/radius-to-meters';
   providers: [
     RentalService,
     GeoUrlApiUtil,
-    // ...unavailabilityProvider,
     MapNewRentalPipe,
     GeoUrlApiPipe,
     RequestCoordinatesPipe,
@@ -52,9 +45,6 @@ import { RadiusToMeters } from './utils/radius-to-meters';
     RentalSearchFilterPipe,
     PricingPipe,
     ValidateEditDetailsPipe,
-    // ProcessUnavailabilityPipe,
-    // ValidateUnavailabilityPipe,
-    // SortUnavailabilityPipe,
     CreateUpdaterDtoPipe,
     ValidateRemoveUnavailabilityPipe,
     AppConfigService,
@@ -66,15 +56,9 @@ import { RadiusToMeters } from './utils/radius-to-meters';
   ],
   exports: [RentalService],
 })
-export class RentalModule implements NestModule {
+export class RentalModule {
   constructor() {
     RentalSchema.index({ loc: '2dsphere' });
   }
-  /**
-   * **summary**: applies the ValidateUpdateUnavailability which validates a user's request to the rental.controller.updateUnavaialability method
-   * @param consumer interfacing defining method for applying user defined middleware to routes
-   */
-  configure = () => {
-    //
-  }
+
 }
