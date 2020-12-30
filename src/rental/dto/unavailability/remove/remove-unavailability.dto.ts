210267538e1939fe643c032c4eb4d701524660f1
Pioneer18 <sellsj14@gmail.com>
2020-12-30 11:28:23 -0500

    removing unnecessary dtos from rental directory

--- src/rental/dto/unavailability/remove/remove-unavailability.dto.ts
+++
@@ -1,9 +0,0 @@
-import { RemoveUnavailabilityInterface } from '../../../../rental/interface/service/remove-unavailability.interface';
-/**
- * **summary**: This Dto contains the data necessary to delte an Unavailability document from the database, it's passed to the rental.controller.removeUnavailability() method
- * - note: [**request payloads**](https://docs.nestjs.com/controllers#request-payloads) are dto classes and not interfaces so they can be accessed by NestJS at runtime
- */
-export class RemoveUnavailabilityDto implements RemoveUnavailabilityInterface {
-  rentalId: string;
-  unavailabilityId: string;
-}
--- src/rental/dto/unavailability/schedule/raw-schedule-unavailability.dto.ts
+++
@@ -1,7 +0,0 @@
-/*import { UnavailabilityDto } from '../unavailability.dto';
- * **summary**: The initial dto submitted from the client to request to schedule Unavailability for a rental. It's passed to the SortUnavailabilityPipe
- * - note: [**request payloads**](https://docs.nestjs.com/controllers#request-payloads) are dto classes and not interfaces so they can be accessed by NestJS at runtime
-export class RawScheduleUnavailabilityDto {
-  unavailability: UnavailabilityDto[];
-}
-*/
