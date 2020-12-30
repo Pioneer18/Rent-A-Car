385e8e3d1e194273e744508111884755445ae999
Pioneer18 <sellsj14@gmail.com>
2020-12-30 11:55:25 -0500

    removing unnecessary code

--- src/rental/interface/service/remove-unavailability.interface.ts
+++
@@ -1,7 +0,0 @@
-/**
- * **summary**: Interface for the rental.service.removeUnavabilability() method
- */
-export interface RemoveUnavailabilityInterface {
-    rentalId: string;
-    unavailabilityId: string;
-}
